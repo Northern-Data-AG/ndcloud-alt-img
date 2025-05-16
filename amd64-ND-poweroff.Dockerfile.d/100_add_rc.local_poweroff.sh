c9f7bbd073e689abca8e11602003c528669860bb
j0ju <j0ju>
2025-05-16 12:45:04 +0000

    amd64-ND-poweroff: dump image that just does a power down (BIOS settings terminiation trigger)

---
+++ amd64-ND-poweroff.Dockerfile.d/100_add_rc.local_poweroff.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -eu
+
+#- files in $0.d will be pupolualated to rootfs in /target
+FSDIR="$0.d"
+if [ -d "$FSDIR" ]; then
+  . "/src/img-mangler.Dockerfile.d/100_add_files.sh"
+else
+  . "$SRC/lib.sh"; init
+fi
+
+chmod 0744 /target/etc/rc.local
